e74d086238955ec79cf28962b5d734b673be3c7f
Semenov V <semvasek@mail.ru>
2025-12-10 13:34:38 +0300

    Добавил логирование

--- HW02/simple-service/src/main/java/ru/vvsem/licensing_service/SimpleServiceApplication.java
+++ HW02/simple-service/src/main/java/ru/vvsem/licensing_service/SimpleServiceApplication.java
@@ -2,11 +2,25 @@ package ru.vvsem.licensing_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class SimpleServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true); // тело запроса (до 10KB по умолчанию)
+        loggingFilter.setIncludeHeaders(false); // можно включить, если нужно
+        loggingFilter.setMaxPayloadLength(10000);
+        loggingFilter.setAfterMessagePrefix("REQUEST DATA: ");
+        return loggingFilter;
     }
 }
